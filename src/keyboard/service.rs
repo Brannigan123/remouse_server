88f501cf79ac28b1a59a5623733ce44427c40b00
Brannigan Sakwah <brannigansakwah@gmail.com>
2022-11-27 22:44:00 +0300

    keyboard actions

---
+++ src/keyboard/service.rs
@@ -0,0 +1,39 @@
+use xdotool::{command::options::KeyboardOption, keyboard};
+
+use super::models::{KeyPress, TextTyping};
+
+pub fn press_key(press: KeyPress) {
+    let mut options = vec![];
+
+    if let Some(clear_modifiers) = press.clear_modifiers && clear_modifiers {
+        options.push(KeyboardOption::ClearModifiers);
+    }
+
+    if let Some(delay) = press.delay {
+        options.push(KeyboardOption::Delay(delay));
+    }
+
+    if let Some(window) = press.window {
+        options.push(KeyboardOption::Window(window));
+    }
+
+    keyboard::send_key(&press.keystroke, xdotool::OptionVec(options));
+}
+
+pub fn type_text(typing: TextTyping) {
+    let mut options = vec![];
+
+    if let Some(clear_modifiers) = typing.clear_modifiers && clear_modifiers {
+        options.push(KeyboardOption::ClearModifiers);
+    }
+
+    if let Some(delay) = typing.delay {
+        options.push(KeyboardOption::Delay(delay));
+    }
+
+    if let Some(window) = typing.window {
+        options.push(KeyboardOption::Window(window));
+    }
+
+    keyboard::type_text(&typing.text, xdotool::OptionVec(options));
+}
